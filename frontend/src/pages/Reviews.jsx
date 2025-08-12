664228d14edf715577f546bfdab1580b1d673ff6
Devansh-debug344 <devanshchaturvedi273@gmail.com>
2025-08-12 10:02:44 +0530

    Update Reviews.jsx

--- frontend/src/pages/Reviews.jsx
+++ frontend/src/pages/Reviews.jsx
@@ -21,7 +21,7 @@ function Reviews({ token }) {
   const fetchReviews = async () => {
     setLoading(true);
     try {
-      const res = await axios.get('https://letterboxd-1.onrender.com/review/', { headers });
+      const res = await axios.get('/review/', { headers });
       setReviews(res.data);
     } catch (error) {
       console.error("Failed to fetch reviews", error);
@@ -35,7 +35,7 @@ function Reviews({ token }) {
     setIsSubmitting(true);
     try {
       await axios.post(
-        'https://letterboxd-1.onrender.com/review/',
+        '/review/',
         { movie_name: movie, review: text, likes: 0 },
         { headers }
       );
@@ -51,7 +51,7 @@ function Reviews({ token }) {
   const updateReview = async (movie_name) => {
     try {
       await axios.patch(
-        'https://letterboxd-1.onrender.com/review/',
+        '/review/',
         {
           movie_name,
           review: editText[movie_name] || '',
@@ -69,7 +69,7 @@ function Reviews({ token }) {
   const deleteReview = async (movie_name) => {
     try {
       await axios.delete(
-        'https://letterboxd-1.onrender.com/review/',
+        '/review/',
         {
           data: { movie_name },
           headers
